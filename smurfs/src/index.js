3b0e5bf754e3617484376397fda5f92aee28a6ba
Akere Adetola <adetolaakere1@gmail.com>
2019-10-11 09:12:35 +0100

    initial state setup

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
 import "./index.css";
 import App from "./components/App";
+import { reducer } from "./state/reducer";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(
+  reducer,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
---
+++ smurfs/src/state/actionCreator.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export const ADD_SMURF = "ADD_SMURF";
+export const ADD_SMURFS = "ADD_SMURFS";
+export const GET_SMURF = "GET_SMURF";
